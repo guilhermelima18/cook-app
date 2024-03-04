224ac22f369c2ab58ca44433182ffad749dbf11c
guilhermelimaflex <guilherme.lima@flexconsulta.com.br>
2024-03-04 19:57:32 -0300

    first commit cook-app

---
+++ src/app/index/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+import { styles } from "./styles";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Hello Cook</Text>
+    </View>
+  );
+}
